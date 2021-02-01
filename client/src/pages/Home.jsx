26ca6aaf15ec567eb94c68d5340279f7b275443e
akonovalov <akonovalov@wallarm.com>
2021-02-01 15:49:20 +0300

    base server and client version

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Layout from './layout/Layout';
+
+const Home = () => {
+    return <Layout>
+        Домашняя страница
+    </Layout>;
+};
+
+export default Home;
+
+
---
+++ client/src/pages/index.js
@@ -0,0 +1,35 @@
+import { Route, Switch, Redirect } from 'react-router-dom';
+import Auth from './auth/Auth';
+import Register from './auth/Register';
+import Leads from './Leads';
+import Home from './Home';
+
+const Pages = ({ isAuthenticated }) => {
+    if (isAuthenticated) {
+        return <Switch>
+            <Route path='/' exact>
+                <Home />
+            </Route>
+
+            <Route path='/leads' exact>
+                <Leads />
+            </Route>
+
+            <Redirect to='/' />
+        </Switch>
+    }
+
+    return <Switch>
+        <Route path='/auth' exact>
+            <Auth />
+        </Route>
+
+        <Route path='/register' exact>
+            <Register />
+        </Route>
+
+        <Redirect to='/auth' />
+    </Switch>
+}
+
+export default Pages;
